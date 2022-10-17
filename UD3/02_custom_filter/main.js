6973152370d35b2f47ce37cadec392925f0d46ec
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-17 18:18:31 +0200

    empezando con U3

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,15 +1,55 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
-}
+// function select(array, condition) {
+//     return array.filter(condition)
+// }
+
+//Opcion 1
+ function select(array, conditionCallback) {
+     let result = []
+
+     for (let index = 0; index < array.length; index++) {
+         const element = array[index];
+
+         if (conditionCallback(element)) {
+             result.push(element)
+         }
+     }
+     return result
+ }
+
+//Opcion 2
+// function select(array, conditionCallback) {
+//     let result = []
+
+//      for (const value of array) {
+//          if (value % 2 === 0) {
+//              result.push(value)
+//          }
+//      }
+
+//     return result
+// }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+
+function pairs(value) {
+    return value % 2 === 0
+}
+
+function impares(value) {
+    return value < 10 && value % 2 === 1
+    
+}
 
 // sólo pares
 assert.deepStrictEqual(select(values, pairs), [2])
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+//assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+//assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+
+// solo impares
+assert.deepStrictEqual(select(values, impares), [3, 5, 7])
--- UD3/03_arrow_custom_filter/main.js
+++ UD3/03_arrow_custom_filter/main.js
@@ -1,15 +1,27 @@
 const assert = require('assert').strict;
 
 function select(array, condition) {
+    let result = []
+      for (const value of array) {
+          if (value % 2 === 0) {
+              result.push(value)
+          }
+      }
+
+     return result
 }
 
 let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+
+//Funcion flecha
+let pairs = (value) => value % 2 === 0;
+
 
 // sólo pares
 assert.deepStrictEqual(select(values, pairs), [2])
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+//assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+//assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])

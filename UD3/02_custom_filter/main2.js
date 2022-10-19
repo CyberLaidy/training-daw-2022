138330260b8c43d3b743f389370f4db27548290a
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-19 20:18:37 +0200

    solucion unidad3 act4

---
+++ UD3/02_custom_filter/main2.js
@@ -0,0 +1,6 @@
+const assert = require('assert').strict;
+
+function olld(value) {
+    return value < 10 && value % 2 === 1
+    
+}
--- UD3/04_iterators/main.js
+++ UD3/04_iterators/main.js
@@ -1,5 +1,8 @@
 'use strict'
 
+const { Console } = require('console');
+
+let map = new Map();
 const assert = require('assert').strict;
 
 var pilots = [
@@ -29,30 +32,56 @@ var pilots = [
   }
 ];
 
-function iterateSimple() {
-}
-function iterateForEach() {
-}
-function mapIds() {
-}
+// //Funciona y Bien
+//  function iterateSimple() {
+//    for (let i = 0; i < pilots.length; i++) {
+//      //me imprime uno o dos
+//      console.log(pilots[i]);
+//    }
+//  }
+//  iterateSimple();
+
+// //Funciona y Bien
+//  function iterateForEach() {
+//    pilots.forEach(element => {
+//      console.log(element);
+//    });
+//  }
+//  iterateForEach();
+
+//Funciona y Bien
+ function mapIds() {
+  return pilots.map(x => x.id)
+ }
+mapIds();
+
+//Funciona y Bien
 function rebels() {
+  return pilots.filter(reb => reb.faction === 'Rebels')
 }
+rebels();
+
+//Funciona y Bien
 function totalFaction(faction) {
+  return pilots.filter(reb => reb.faction === faction).length
 }
+
+//
 function avgYears(faction) {
+  return pilots .filter(x => x.faction === faction).reduce((a, b) => a + b.years, 0) / pilots.filter(x => x.faction === faction).length
+
 }
+avgYears();
 
-// use console.log
-iterateSimple()
-iterateForEach()
-try {
-  assert.deepStrictEqual(mapIds(), [2,8,40,66])
-  assert.deepStrictEqual(rebels(), [pilots[0], pilots[3]])
+// use console.log 
+// try {
+//   assert.deepStrictEqual(mapIds(), [2,8,40,66])
+//   assert.deepStrictEqual(rebels(), [pilots[0], pilots[3]])
 
-  assert.deepStrictEqual(totalFaction('Rebels'), 2)
+//   assert.deepStrictEqual(totalFaction('Rebels'), 2)
 
-  assert.deepStrictEqual(avgYears('Rebels'), 22.5)
-  assert.deepStrictEqual(avgYears('Empire'), 25)
-} catch (error) {
-  console.error(error)
-}
+//   assert.deepStrictEqual(avgYears('Rebels'), 22.5)
+//   assert.deepStrictEqual(avgYears('Empire'), 25)
+// } catch (error) {
+//   console.error(error)
+// }

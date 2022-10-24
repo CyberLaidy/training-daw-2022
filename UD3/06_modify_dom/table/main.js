cfc19f8248d7f2e278e72dbe6e73cc670ead5bea
DESKTOP-S6DAD6J\A8-PC100 <asd@svalero.com>
2022-10-24 18:09:59 +0200

    clase lunes 24/10 ud3 act6

--- UD3/06_modify_dom/table/main.js
+++ UD3/06_modify_dom/table/main.js
@@ -1,6 +1,17 @@
 function onClick() {
     let tbody = document.getElementsByTagName('tbody')[0]
 
+    //Hacer que el ID se autoincremente (mediante una variable)
+    
+    //Añadir una columna 'Acciones' en el HTML
+    
+    //Añadir a los datos que se incluyan cada vez que se pulse un botón
+    
+    //un enlace a la url 'http://localhost/edit/id'
+
+
+
+
     let tr = document.createElement('tr')
     let td = document.createElement('td')
     td.innerText = '0'

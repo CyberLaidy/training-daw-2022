a7606afa64b23866f63ddf2752008c6f065c7a93
SANDARA\Sandra <a25660@svalero.com>
2022-10-26 22:41:56 +0200

    cambios U3 Act 6.1

--- UD3/06_modify_dom/exercise01/main.js
+++ UD3/06_modify_dom/exercise01/main.js
@@ -1,5 +1,42 @@
 window.onload = function(e) {
     console.log('documento cargado')
 
-    // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+    // // document.getElementsByTagName('h1')[0].innerText = 'Changed from JS!!'
+
+    //Cambiar el título del segundo h2
+    let ti2 = document.getElementsByTagName("h2").innerText=('Hola')
+    console.log(ti2);
+
+    //Seleccionar el elemento con id == username
+    let idUser = document.getElementById("username");
+    console.log(idUser);
+
+    //Cambiar el color de todos los .first que estén dentro de un artículo 
+    let colorF = document.querySelectorAll("article.first");
+    function colorF() {
+        Element.style.color="orange";
+    }
+    console.log(colorF);
+
+    //Seleccionar todos lo elementos li con class == item
+    let selectLiClassItem = document.getElementsByClassName("li.item");
+    console.log(selectLiClassItem);
+
+    //Seleccionar todos lo buttons dentro de class == buttons
+    let selectClassBt = document.querySelectorAll("class.buttons");
+    console.log(selectClassBt);
+
+    //Cambiar el color de fondo del primer párrafo
+    let fp = document.querySelector("p").getElementsByClassName("first");
+    function fp() {
+        Element.style.backgroundColor="pink";
+    }
+    console.log(fp);
+
+    //Cambiar el color de frente (forecolor) de los elementos buttons dentro de class == buttons
+    let changeColor = document.getElementsByClassName("buttons");
+    function changeColor() {
+        Element.style.forecolor="gray";
+    }
+    console.log(changeColor);
 }
